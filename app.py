35eece34bd5fcf2c7099325fb0b00d85758a0879
younkeun <coyek@naver.com>
2024-12-15 17:12:05 +0900

    12/15 수정-2

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def gallery():
     return render_template('gallery.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
